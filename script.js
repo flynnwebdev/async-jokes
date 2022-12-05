2bc605a49d3f69702499221092df92dab12a2d34
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2022-12-05 11:13:35 +1000

    Init node and use roll package

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-function getJoke(cb) {
-    req = new XMLHttpRequest()
-    req.addEventListener('load', event => cb(event.target.response.joke))
-    req.open('GET', 'https://icanhazdadjoke.com/')
-    req.setRequestHeader('Accept', 'application/json')
-    req.responseType = 'json'
-    req.send()
-}
+import { add, PI } from './math.js'
+import Roll from 'roll'
 
-const jokes = []
+// // console.log(foo)
+console.log(add(5, 7))
 
-getJoke(joke => {
-    jokes.push(joke)
-    getJoke(joke => {
-        jokes.push(joke)
-        getJoke(joke => {
-            jokes.push(joke)
-            console.log(jokes)
-        })
-    })
-})
-
+const roll = new Roll()
+console.log(roll.roll('2d10'))
